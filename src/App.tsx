53bda52d540ec1c84d65719c97521c12022559e7
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-12-07 01:02:58 -0300

    feat: add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'expo-status-bar';
 
 import Welcome from './components/Welcome';
 
+import theme from './global/styles/theme';
+
 function App() {
   return (
-    <View style={styles.container}>
-      <Welcome name="José Eduardo" />
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Welcome name="José Eduardo" />
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
